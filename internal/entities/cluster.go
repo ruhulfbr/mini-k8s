cd657eba181828e00b3aa5d5d3c2df512381560f
Md Ruhul Amin <ruhul11bd@gmail.com>
2026-01-20 13:52:30 +0600

    Reformat entities

--- internal/entities/cluster.go
+++ internal/entities/cluster.go
@@ -21,25 +21,25 @@ const (
 )
 
 type Cluster struct {
-	Id              int64         `db:"id"              json:"id"`
-	ApplicationId   int64         `db:"application_id"  json:"application_id"`
-	Name            string        `db:"name"            json:"name"`
-	IP              string        `db:"ip"              json:"ip"`
-	Port            int           `db:"port"              json:"port"`
-	Replicas        int           `db:"replicas"        json:"replicas"`
-	CPU             int           `db:"cpu"       json:"cpu"`
-	Memory          int           `db:"memory"       json:"memory"`
-	Path            string        `db:"path"            json:"path"`
-	Type            ClusterType   `db:"type"            json:"type"`
-	DeployMode      DeployMode    `db:"deploy_mode"          json:"deploy_mode"`
+	Id              int64         `db:"id" json:"id"`
+	ApplicationId   int64         `db:"application_id" json:"application_id"`
+	Name            string        `db:"name" json:"name"`
+	IP              string        `db:"ip" json:"ip"`
+	Port            int           `db:"port" json:"port"`
+	Replicas        int           `db:"replicas" json:"replicas"`
+	CPU             int           `db:"cpu" json:"cpu"`
+	Memory          int           `db:"memory" json:"memory"`
+	Path            string        `db:"path" json:"path"`
+	Type            ClusterType   `db:"type" json:"type"`
+	DeployMode      DeployMode    `db:"deploy_mode" json:"deploy_mode"`
 	Image           *string       `db:"image" json:"image"`
 	Envs            *string       `db:"envs" json:"envs"`
 	CurrentImageTag *string       `db:"current_image_tag" json:"current_image_tag"`
-	CurrentVersion  *string       `db:"current_version"   json:"current_version"`
-	Status          ClusterStatus `db:"status"          json:"status"`
+	CurrentVersion  *string       `db:"current_version" json:"current_version"`
+	Status          ClusterStatus `db:"status" json:"status"`
 	LastDeployedAt  *time.Time    `db:"last_deployed_at" json:"last_deployed_at"`
-	CreatedAt       time.Time     `db:"created_at"      json:"created_at"`
-	UpdatedAt       time.Time     `db:"updated_at"      json:"updated_at"`
+	CreatedAt       time.Time     `db:"created_at" json:"created_at"`
+	UpdatedAt       time.Time     `db:"updated_at" json:"updated_at"`
 }
 
 func ClusterFromRequest(appId int64, req *requests.CreateClusterRequest, clusterId ...int64) (*Cluster, error) {
--- internal/entities/pod.go
+++ internal/entities/pod.go
@@ -4,18 +4,11 @@ import (
 	"time"
 )
 
-type PodStatus int
-
-const (
-	PodPending PodStatus = 0
-	PodRunning PodStatus = 1
-)
-
 type Pod struct {
-	Id            int64     `db:"id"         json:"id"`
+	Id            int64     `db:"id" json:"id"`
 	ClusterId     int64     `db:"cluster_id" json:"cluster_id"`
 	ContainerId   string    `db:"container_id" json:"container_id"`
 	ContainerName string    `db:"container_name" json:"container_name"`
 	IpAddress     string    `db:"ip_address" json:"ip_address"`
-	CreatedAt     time.Time `db:"created_at"  json:"created_at"`
+	CreatedAt     time.Time `db:"created_at" json:"created_at"`
 }
