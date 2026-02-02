2bbc2e22729d0d586c3181a603690086a112fd0d
ruhulfbr <ruhul11bd@gmail.com>
2026-02-02 22:28:56 +0600

    Refactore some coddes

--- internal/http/requests/deploy_request.go
+++
@@ -1,8 +0,0 @@
-package requests
-
-type DeployRequest struct {
-	ServiceName string `json:"service_name"`
-	Image       string `json:"image"`
-	Replicas    int    `json:"replicas"`
-	Port        int    `json:"port"`
-}
